22a31e0d40c60a48f44ae7bac6981b996339f8dc
Neil White <variableresistor@gmail.com>
2021-04-26 17:50:13 -0700

    Added Get-FirstRepeatedCharacter.ps1 and Pester tests

---
+++ Get-FirstRepeatedCharacter.Tests.ps1
@@ -0,0 +1,35 @@
+﻿Describe "Get-FirstRepeatedCharacter.ps1" {
+    Context "Using 'Pretty' method" {
+        It "Outputs a 'c' in an unordered string" {
+            . "$PSScriptRoot\Get-FirstRepeatedCharacter.ps1" -InputString "eccdr" -Method Pretty | Should -Be "c"
+        }
+
+        It "Outputs a 'c' only when multiple characters repeat within the string" {
+            . "$PSScriptRoot\Get-FirstRepeatedCharacter.ps1" -InputString "ecddrra" -Method Pretty | Should -Be "d"
+        }
+
+        It "Is case sensitive" {
+            . "$PSScriptRoot\Get-FirstRepeatedCharacter.ps1" -InputString "ecDdra" -Method Pretty | Should -BeNullOrEmpty
+        }
+    }
+    Context "Using 'Dotnet' method" {
+        It "Outputs a 'c' in an unordered string" {
+            . "$PSScriptRoot\Get-FirstRepeatedCharacter.ps1" -InputString "eccdr" -Method Dotnet | Should -Be "c"
+        }
+
+        It "Outputs a 'c' only when multiple characters repeat within the string" {
+            . "$PSScriptRoot\Get-FirstRepeatedCharacter.ps1" -InputString "ecddrra" -Method Dotnet | Should -Be "d"
+        }
+
+        It "Is case sensitive" {
+            . "$PSScriptRoot\Get-FirstRepeatedCharacter.ps1" -InputString "ecDdra" -Method Dotnet | Should -BeNullOrEmpty
+        }
+    }
+
+    It "Has certain parameters and properties" {
+        $CommandInfo = Get-Command "$PSScriptRoot\Get-FirstRepeatedCharacter.ps1"
+
+        $CommandInfo | Should -HaveParameter InputString -Type ([System.String]) -Mandatory
+        $CommandInfo | Should -HaveParameter Method -Type ([System.String])
+    }
+}
---
+++ Get-FirstRepeatedCharacter.ps1
@@ -0,0 +1,52 @@
+﻿<#
+.SYNOPSIS
+    Returns the first recurring character in from the input string.
+.PARAMETERS Input
+    String to return the first recurring character from.
+.PARAMETERS Method
+    The method used to return the first repeating character
+.LINK
+    https://powershellexplained.com/2018-10-15-Powershell-arrays-Everything-you-wanted-to-know/
+.AUTHOR
+    Neil White
+#>
+[CmdletBinding()]
+[OutputType([string])]
+param
+(
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$InputString
+
+    ,[ValidateSet("Pretty","Dotnet")]
+    [string]$Method = "Pretty"
+)
+#requires -Version 5 -PSEdition Desktop
+
+process
+{
+    $CharArray = $InputString.ToCharArray()
+
+    # The Switch statement in Windows PowerShell is way slower than in Core, so use if, elseif structure
+    if ($Method -eq "Pretty")
+    {
+        $CharsAlreadyTried = @()
+
+        foreach ($Char in $CharArray)
+        {
+            if ($Char -cin $CharsAlreadyTried) { return $Char }
+            else { $CharsAlreadyTried += $Char }
+        }
+    }
+    elseif ($Method -eq "Dotnet")
+    {
+        $CharsAlreadyTried = [System.Collections.Generic.List[string]]::new()
+
+        foreach ($Char in $CharArray)
+        {
+            if ($CharsAlreadyTried.Contains($Char)) { return $Char }
+            else { $CharsAlreadyTried.Add($Char) }
+        }
+    }
+    else { throw "Method $Method was invalid" }
+}
